6e9f2688c96e34fc543cb9e431272c8d4d3ba7ec
EChilin5 <edgar3ac@gmail.com>
2024-05-07 14:09:15 -0700

    add error handleing to mogoose

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ const port = process.env.PORT || 4000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(port, "0.0.0.0", () => {
-      console.log("listening to database", port);
-    });
+    app
+      .listen(port, "0.0.0.0", () => {
+        console.log(`Server listening on port ${port}`);
+      })
+      .on("error", (err) => {
+        console.error("Error starting the server:", err);
+      });
   })
   .catch((error) => {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
   });
