a903c3617349db4ad3a57b207cd5c6ec18e4b953
EChilin5 <edgar3ac@gmail.com>
2024-05-07 13:48:59 -0700

    update port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 
 // connect to db
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
